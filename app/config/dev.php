2d9cdaad99bfa2436aba1abee10604ea732da400
Naeva <naeva.mallet@gmail.com>
2017-12-20 00:35:34 +0100

    Ajout de tests fonctionnel

--- app/config/dev.php
+++ app/config/dev.php
@@ -4,8 +4,17 @@
  * Date: 16/12/2017
  */
 
-// inclu la configuration de prodduction
-require __DIR__.'/prod.php';
+
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => '127.0.0.1',  // Mandatory for PHPUnit testing
+    'port'     => '3307',
+    'dbname'   => 'mescontacts',
+    'user'     => 'mescontacts_user',
+    'password' => 'secret',
+);
 
 // active le mode debug
 $app['debug'] = true;
---
+++ tests/Tests/AppTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * User: Naeva
+ * Date: 19/12/2017
+ */
+
+namespace MesContacts\Tests;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use Silex\WebTestCase;
+
+/**
+ * Class AppTest
+ * @package MesContacts\Tests
+ */
+class AppTest extends WebTestCase
+{
+
+    /**
+     * Vérifie que toutes les URL d'application se chargent avec succès.
+     * Pendant l'exécution du test, cette méthode est appelée pour chaque URL renvoyée par la méthode provideUrls.
+     *
+     * @dataProvider provideUrls
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = $this->createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createApplication()
+    {
+        $app = new \Silex\Application();
+
+        require __DIR__.'/../../app/config/dev.php';
+        require __DIR__.'/../../app/app.php';
+        require __DIR__.'/../../app/routes.php';
+
+        // Génère des exceptions brutes plutôt que des pages html si des erreurs se produisent
+        unset($app['exception_handler']);
+        // Simule une session pour les tests
+        $app['session.test'] = true;
+        // Active la connexion annonyme
+        $app['security.access_rules'] = array();
+
+        return $app;
+    }
+
+    /**
+     * Fournit toutes les URL valide de l'application.
+     *
+     * @return array
+     */
+    public function provideUrls()
+    {
+        return array(
+            array('/'),
+            array('/login'),
+            array('/contact/1'),
+            array('/contact/add'),
+            array('/contact/1/edit'),
+            array('/adresse/1/add'),
+            array('/adresse/1/edit')
+        );
+    }
+}
